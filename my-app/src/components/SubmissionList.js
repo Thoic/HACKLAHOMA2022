62460a99f844fa1a8365fc26889ff3212dd9bd62
Nick <nicholas.j.thompson-1@ou.edu>
2022-02-13 04:40:53 -0600

    added sample files

--- my-app/src/components/SubmissionList.js
+++ my-app/src/components/SubmissionList.js
@@ -6,28 +6,49 @@ class SubmissionList extends Component {
       super(props);
       this.state = {
          submissions: [
-            {
-               title: "myron 2",
-               runtime: 134,
-               tags: ["music", "hip-hop", "fleetness"],
-               path: "/myron 2.mp3",
-               image: "/myron2.png",
+             {
+                 title: "一剪梅",
+                 runtime: 223,
+                 tags: ["music", "chinese", "陳信義", "egg"],
+                 path: "/yijianmei.mp3",
+                 image: "/yijianmei.jpg",
+             },
+             {
+                title: "Nature Sounds",
+                runtime: 60,
+                tags: ["nature", "stream", "relaxing"],
+                path: "/nature.mp3",
+                image: "/nature.jpg",
             },
             {
-               title: "my-audio-two",
-               runtime: 145,
-               tags: ["music"],
-               path: null,
-               image: null,
+                title: "One Dance",
+                runtime: 173,
+                tags: ["drake", 'hip-hop', '2016'],
+                path: "/drake.mp3",
+                image: "/drake.jpg",
             },
             {
-               title: "myron myron myron myron yron myron myron myron",
-               runtime: 134,
-               tags: ["music", "hip-hop", "fleetness"],
-               path: "/myron 2.mp3",
-               image: "/myron2.png",
+                title: "Treat You Better",
+                runtime: 187,
+                tags: ["shawn mendes", "pop", '2016', 'disney'],
+                path: "/mende.mp3",
+                image: "/mendes.jpg",
             },
-         ],
+            {
+                title: "Biden Speech",
+                runtime: 124,
+                tags: ["joe biden", "2022", "speech", "politics", "usa"],
+                path: "/biden.mp3",
+                image: "/biden.jpg",
+            },
+            {
+                title: "Buffer",
+                runtime: 111,
+                tags: [],
+                path: null,
+                image: null
+            }
+         ]
       };
    }
 
